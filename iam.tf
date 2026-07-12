ac7043eb872e4d30e5b4d791d2431abadc8637fc
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-07-12 16:09:17 -0400

    Add IAM role policy to allow read from Secrets Manager

--- iam.tf
+++ iam.tf
@@ -16,6 +16,9 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     ]
   })
 
+  tags = { Name = "${var.project_name}-ECSTaskExecutionRole" }
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_managed" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
@@ -34,3 +37,27 @@ resource "aws_iam_role" "ecs_task_role" {
       }
     ]
   })
+
+  tags = { Name = "${var.project_name}-ECSTaskRole" }
+}
+
+resource "aws_iam_role_policy" "ecs_task_secrets_read" {
+  name = "${var.project_name}-secrets-read"
+  role = aws_iam_role.ecs_task_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "secretsmanager:GetSecretValue"
+        Resource = "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.current.account_id}:secret:${var.project_name}/*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "kms:Decrypt"
+        Resource = "arn:aws:kms:${var.aws_region}:${data.aws_caller_identity.current.account_id}:key/alias/aws/secretsmanager"
+      }
+    ]
+  })
+}
